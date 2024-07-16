6d653791493cc5f7b4658d153794e1373ea428cd
Philip Smolen <philip@trade-ideas.com>
2024-07-16 01:29:46 -0700

    Create an outline of the document.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,4 @@
 // This is the preferred way to include a css file.
 import "./style.css";
 
-import { getById } from "phil-lib/client-misc";
-
-// getById() is usually the first function I import.
-// This is the best way to access HTML from TypeScript most of the time.
-const typescriptStatusSpan = getById("typescriptStatus", HTMLSpanElement);
-typescriptStatusSpan.innerText = "working";
+//import { getById } from "phil-lib/client-misc";

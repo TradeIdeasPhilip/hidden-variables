b1648dde163eb27a8211d55df47f05bea22b2860
Philip Smolen <philip@trade-ideas.com>
2024-07-19 01:08:44 -0700

    You can now set the rotation around the Y axis.
    So you can see different amounts of the two colors.
    And so you can see the effect of the other rotation.
    
    I added several examples to the bottom of the document.
    Including some animations.
    
    There are some bugs.
    See the new TODO items.
    But overall it looks great.
    I am finally confident that these will suit my purpose.

--- src/main.ts
+++ src/main.ts
@@ -2,23 +2,27 @@
 import "./style.css";
 
 import { getById } from "phil-lib/client-misc";
-
+import { positiveModulo } from "./utility";
+import { initializedArray } from "phil-lib/misc";
 
 class Sphere {
   /**
-   * 
+   * **TODO** fix this slightly out of date but not completely wrong documentation:
    * @param coverage How far the highlight should move to the right.
    * - 0 means none of the highlight shows.
    * - 1 means the highlight covers the entire circle.
    * - 0.5 means to cover exactly the left half with the highlight.
    * - 0.1 would highlight a small crescent on the left.
-   * 
+   *
    * Strictly speaking, `coverage` is a ratio to say how much area of the _circle_ is being covered.
    * You'll have to do some math first if you want to simulate a rotating `sphere`.
    * @throws `coverage` must be between 0 and 1 (inclusive) or this will throw an exception.
    */
-  static createHighlightFromLeft(coverage : number) {
-    if (coverage < 0 || coverage > 1 || !isFinite(coverage)) {
+  static createHighlight(a: number, b: number) {
+    if (a < 0 || a > 1 || !isFinite(a)) {
+      throw new Error("wtf");
+    }
+    if (b < 0 || b > 1 || !isFinite(b)) {
       throw new Error("wtf");
     }
     const radius = 0.5;
@@ -26,53 +30,168 @@ class Sphere {
     const bottom = `0,${radius}`;
     const clockwise = "1";
     const counterclockwise = "0";
-    const returningRadius = Math.abs(coverage-radius);
-    const  returningDirection=(coverage <= 0.5)?clockwise:counterclockwise;
-    const d = `M ${top} A ${radius},${radius} 3.14159 0 ${counterclockwise} ${bottom} A ${returningRadius},${radius} 3.14159 0 ${returningDirection} ${top}`;
+    const radiusA = Math.abs(a - radius);
+    const directionA = a >= 0.5 ? clockwise : counterclockwise;
+    const radiusB = Math.abs(b - radius);
+    const directionB = b <= 0.5 ? clockwise : counterclockwise;
+    const d = `M ${top} A ${radiusA},${radius} 3.14159 0 ${directionA} ${bottom} A ${radiusB},${radius} 3.14159 0 ${directionB} ${top}`;
     return d;
   }
   readonly #top = document.createElementNS("http://www.w3.org/2000/svg", "g");
-  get top() {return this.#top;}
-  readonly #orange = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-  readonly #white = document.createElementNS("http://www.w3.org/2000/svg", "path");
-  readonly #lightAndShadow = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+  get top() {
+    return this.#top;
+  }
+  readonly #orange = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "circle"
+  );
+  readonly #white = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "path"
+  );
+  readonly #lightAndShadow = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "circle"
+  );
   constructor() {
     this.#top.append(this.#orange, this.#white, this.#lightAndShadow);
-    this.#top.classList.add("sphere")
+    this.#top.classList.add("sphere");
   }
   #yAngle = 0;
-  get yAngle() {return this.#yAngle;}
+  get yAngle() {
+    return this.#yAngle;
+  }
+  /**
+   * - 0 means the orange side is facing directly toward the user.
+   * - π means the white side is facing directly toward the user.
+   * - A very small positive number will show a very small sliver of white on the left.
+   * - A number slightly below zero will show a very small sliver of white on the right.
+   * - π / 2 will be half orange and half white, with the white on the left.
+   * - 3 * π / 2 will be half orange and half white, with the white on the right.
+   * - A fixed sized change in the input will cause the most obvious changes to the display when we are displaying half of each color.
+   * - It will cause the least obvious changes when we are displaying mostly one color and very little of the other.
+   */
+  set yAngle(newValue) {
+    const clampedAngle = positiveModulo(newValue, 2 * Math.PI);
+    if (clampedAngle < Math.PI) {
+      // The white part is on the left.
+      /**
+       * As clampedAngle moves from 0 to π, adjustedForSpeed moves from 0 to 1.
+       * The function is not linear.
+       */
+      const right = (1 - Math.cos(clampedAngle)) / 2;
+      this.updateWhite(0, right);
+    } else {
+      // The white part is on the right
+      /**
+       * As clampedAngle moves from π to 2π, adjustedForSpeed moves from 0 to 1.
+       * The function is not linear.
+       */
+      const left = (1 + Math.cos(clampedAngle)) / 2;
+      this.updateWhite(left, 1);
+    }
+  }
   #zAngle = 0;
-  get zAngle() {return this.#zAngle;}
+  get zAngle() {
+    return this.#zAngle;
+  }
   set zAngle(newValue) {
-    this.#white.style.transform=`rotate(${newValue}rad)`;
+    this.#white.style.transform = `rotate(${newValue}rad)`;
   }
   #x = 0;
-  get x() {return this.#x;}
+  get x() {
+    return this.#x;
+  }
   set x(newValue) {
     this.#x = newValue;
     this.#top.style.setProperty("--x", newValue + "px");
   }
   #y = 0;
-  get y() {return this.#y;}
+  get y() {
+    return this.#y;
+  }
   set y(newValue) {
     this.#y = newValue;
     this.#top.style.setProperty("--y", newValue + "px");
   }
   #diameter = 1;
-  get diameter() {return this.#diameter;}
+  get diameter() {
+    return this.#diameter;
+  }
   set diameter(newValue) {
     this.#diameter = newValue;
     this.#top.style.setProperty("--diameter", newValue.toString());
   }
+  updateWhite(a: number, b: number) {
+    const d = Sphere.createHighlight(a, b);
+    this.#white.setAttribute("d", d);
+    return d; // for debugging, not production
+  }
 }
 
-console.log(Sphere.createHighlightFromLeft(5/6));
-
 const testSvg = getById("test", SVGSVGElement);
-const sphere = new Sphere();
-testSvg.appendChild(sphere.top);
-(window as any).sphere = sphere;
-sphere.diameter = 2;
-sphere.y = 2;
-sphere.x = 1;
+const spheres = initializedArray(5, (index) => {
+  const sphere = new Sphere();
+  testSvg.appendChild(sphere.top);
+  switch (index) {
+    case 0: {
+      sphere.diameter = 2;
+      sphere.y = 2;
+      sphere.x = 1;
+      sphere.yAngle = Math.PI / 4;
+      break;
+    }
+    case 1: {
+      sphere.y = 1.5;
+      sphere.x = 2.5;
+      sphere.yAngle = Math.PI + 0.25;
+      break;
+    }
+    case 2: {
+      sphere.y = 2.5;
+      sphere.x = 2.5;
+      sphere.yAngle = Math.PI / 2 + 0.25;
+      break;
+    }
+    case 3: {
+      sphere.y = 0.5;
+      sphere.x = 3.5;
+      break;
+    }
+    case 4: {
+      sphere.y = 1.5;
+      sphere.x = 3.5;
+      break;
+    }
+    default: {
+      throw new Error("wtf");
+    }
+  }
+  return sphere;
+});
+(window as any).spheres = spheres;
+
+function animateSoon() {
+  requestAnimationFrame(animate);
+}
+
+function animate(timestamp: number) {
+  animateSoon();
+  spheres[0].zAngle = timestamp / 700;
+  spheres[3].yAngle = timestamp / 831;
+  spheres[4].zAngle = timestamp / 607;
+  spheres[4].yAngle = timestamp / 501;
+}
+
+animateSoon();
+// TODO fix these bugs:
+// spheres[3].yAngle = timestamp / 831; shows a bug.
+//   The animation jumps every time the diving line crosses over the half way point.
+//   The animation is smooth at all other times.
+// The white never properly covers the orange right.
+//   It's fine where the orange and white are supposed to be touching.
+//   But where the white is touching the background color, you always see a little orange poking free.
+// Sometimes the crescent shape has some weird effects.
+//   Notice the little green line just to the left of the top of the the green crescent.
+//   And look at the two spheres below it.  A little white bit of the bottom sphere is covering the sphere immediately above it.
+//   The two spheres which are rotating about the y axis show the same bug in a constantly changing way, sometime huge.
--- src/utility.ts
+++ src/utility.ts
@@ -13,3 +13,25 @@ export function lerp(at0: number, at1: number, where: number) {
   return at0 + (at1 - at0) * where;
 }
 
+/**
+ * This is similar to `numerator % denominator`, i.e. modulo division.
+ * The difference is that the result will never be negative.
+ * If the numerator is negative `%`  will return a negative number.
+ *
+ * If the 0 point is chosen arbitrarily then you should use `positiveModulo()` rather than `%`.
+ * For example, C's `time_t` and JavaScript's `Date.prototype.valueOf()` say that 0 means midnight January 1, 1970.
+ * Negative numbers refer to times before midnight January 1, 1970, and positive numbers refer to times after midnight January 1, 1970.
+ * But midnight January 1, 1970 was chosen arbitrarily, and you probably don't want to treat times before that differently than times after that.
+ * And how many people would even think to test a negative date?
+ *
+ * `positiveModulo(n, d)` will give the same result as `positiveModulo(n + d, d)` for all vales of `n` and `d`.
+ * (You might get 0 sometimes and -0 other times, but those are both `==` so I'm not worried about that.)
+ */
+export function positiveModulo(numerator: number, denominator: number) {
+  const simpleAnswer = numerator % denominator;
+  if (simpleAnswer < 0) {
+    return simpleAnswer + Math.abs(denominator);
+  } else {
+    return simpleAnswer;
+  }
+}
